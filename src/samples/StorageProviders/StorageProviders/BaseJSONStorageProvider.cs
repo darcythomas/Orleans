fbc61285d872ad966b4e26b51680174fe47a94de
Niklas Gustafsson <niklasg@microsoft.com>
2014-03-07 11:40:50 -0800

    Adding storage provider samples

---
+++ src/samples/StorageProviders/StorageProviders/BaseJSONStorageProvider.cs
@@ -0,0 +1,158 @@
+﻿//*********************************************************
+//    Copyright (c) Microsoft. All rights reserved.
+//    
+//    Apache 2.0 License
+//    
+//    You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+//    
+//    Unless required by applicable law or agreed to in writing, software 
+//    distributed under the License is distributed on an "AS IS" BASIS, 
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+//    implied. See the License for the specific language governing 
+//    permissions and limitations under the License.
+//
+//*********************************************************
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+using Orleans;
+using Orleans.Storage;
+using Orleans.Providers;
+
+namespace Samples.StorageProviders
+{
+    /// <summary>
+    /// Base class for JSON-based grain storage providers.
+    /// </summary>
+    public abstract class BaseJSONStorageProvider : IStorageProvider
+    {
+        /// <summary>
+        /// Logger object
+        /// </summary>
+        public OrleansLogger Log
+        {
+            get; protected set;
+        }
+
+        /// <summary>
+        /// Storage provider name
+        /// </summary>
+        public string Name { get; protected set; }
+
+        /// <summary>
+        /// Data manager instance
+        /// </summary>
+        /// <remarks>The data manager is responsible for reading and writing JSON strings.</remarks>
+        protected IJSONStateDataManager DataManager { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public BaseJSONStorageProvider()
+        {
+        }
+
+        /// <summary>
+        /// Initializes the storage provider.
+        /// </summary>
+        /// <param name="name">The storage provider name.</param>
+        /// <param name="storageProviderManager">A Orleans runtime object managing all storage providers.</param>
+        public virtual Task Init(string name, IProviderRuntime providerRuntime, IProviderConfiguration config)
+        {
+            Log = providerRuntime.GetLogger(this.GetType().FullName, Logger.LoggerType.Application);
+            return TaskDone.Done;
+        }
+
+        /// <summary>
+        /// Closes the storage provider during silo shutdown.
+        /// </summary>
+        /// <returns></returns>
+        public Task Close()
+        {
+            if (DataManager != null)
+                DataManager.Dispose();
+            DataManager = null;
+            return TaskDone.Done;
+        }
+
+        /// <summary>
+        /// Reads persisted state from the backing store and deserializes it into the the target
+        /// grain state object.
+        /// </summary>
+        /// <param name="grainType">A string holding the name of the grain class.</param>
+        /// <param name="grainId">Represents the long-lived identity of the grain.</param>
+        /// <param name="grainState">A reference to an object to hold the persisted state of the grain.</param>
+        /// <returns></returns>
+        public async Task ReadStateAsync(string grainType, GrainId grainId, IGrainState grainState)
+        {
+            if (DataManager == null) throw new ArgumentException("DataManager property not initialized");
+            var entityData = await DataManager.Read(grainState.GetType().Name, grainId.ToKeyString());
+            if (entityData != null)
+            {
+                ConvertFromStorageFormat(grainState, entityData);
+            }
+        }
+
+        /// <summary>
+        /// Writes the persisted state from a grain state object into its backing store.
+        /// </summary>
+        /// <param name="grainType">A string holding the name of the grain class.</param>
+        /// <param name="grainId">Represents the long-lived identity of the grain.</param>
+        /// <param name="grainState">A reference to an object holding the persisted state of the grain.</param>
+        /// <returns></returns>
+        public Task WriteStateAsync(string grainType, GrainId grainId, IGrainState grainState)
+        {
+            if (DataManager == null) throw new ArgumentException("DataManager property not initialized");
+            var entityData = ConvertToStorageFormat(grainState);
+            return DataManager.Write(grainState.GetType().Name, grainId.ToKeyString(), entityData);
+        }
+
+        /// <summary>
+        /// Removes grain state from its backing store, if found.
+        /// </summary>
+        /// <param name="grainType">A string holding the name of the grain class.</param>
+        /// <param name="grainId">Represents the long-lived identity of the grain.</param>
+        /// <param name="grainState">An object holding the persisted state of the grain.</param>
+        /// <returns></returns>
+        public Task ClearStateAsync(string grainType, GrainId grainId, GrainState grainState)
+        {
+            if (DataManager == null) throw new ArgumentException("DataManager property not initialized");
+            DataManager.Delete(grainState.GetType().Name, grainId.ToKeyString());
+            return TaskDone.Done;
+        }
+
+        /// <summary>
+        /// Serializes from a grain instance to a JSON document.
+        /// </summary>
+        /// <param name="grainState"></param>
+        /// <param name="entity"></param>
+        /// <remarks>
+        /// See:
+        /// http://msdn.microsoft.com/en-us/library/system.web.script.serialization.javascriptserializer.aspx
+        /// for more on the JSON serializer.
+        /// </remarks>
+        protected static string ConvertToStorageFormat(IGrainState grainState)
+        {
+            Dictionary<string, object> dataValues = grainState.AsDictionary();
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            return serializer.Serialize(dataValues);
+        }
+
+        /// <summary>
+        /// Constructs a grain state instance by deserializing a JSON document.
+        /// </summary>
+        /// <param name="grainState"></param>
+        /// <param name="entity"></param>
+        protected static void ConvertFromStorageFormat(IGrainState grainState, string entityData)
+        {
+            JavaScriptSerializer deserializer = new JavaScriptSerializer();
+            object data = deserializer.Deserialize(entityData, grainState.GetType());
+            var dict = ((IGrainState)data).AsDictionary();
+            grainState.SetAll(dict);
+        }
+    }
+
+}
---
+++ src/samples/StorageProviders/StorageProviders/FileStorageProvider.cs
@@ -0,0 +1,150 @@
+﻿//*********************************************************
+//    Copyright (c) Microsoft. All rights reserved.
+//    
+//    Apache 2.0 License
+//    
+//    You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+//    
+//    Unless required by applicable law or agreed to in writing, software 
+//    distributed under the License is distributed on an "AS IS" BASIS, 
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+//    implied. See the License for the specific language governing 
+//    permissions and limitations under the License.
+//
+//*********************************************************
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Services.Client;
+using System.Data.Services.Common;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+using Orleans;
+using Orleans.Providers;
+
+namespace Samples.StorageProviders
+{
+    /// <summary>
+    /// Orleans storage provider implementation for file-backed stores.
+    /// </summary>
+    /// <remarks>
+    /// The storage provider should be included in a deployment by adding this line to the Orleans server configuration file:
+    /// 
+    ///     <Provider Type="Samples.StorageProviders.OrleansFileStorage" Name="FileStore" RooDirectory="SOME FILE PATH" />
+    ///
+    /// and this line to any grain that uses it:
+    /// 
+    ///     [StorageProvider(ProviderName = "FileStore")]
+    /// 
+    /// The name 'FileStore' is an arbitrary choice.
+    /// 
+    /// Note that unless the root directory path is a network path available to all silos in a deployment, grain state
+    /// will not transport from one silo to another.
+    /// </remarks>
+    public class OrleansFileStorage : BaseJSONStorageProvider
+    {
+        /// <summary>
+        /// The directory path, relative to the host of the silo. Set from
+        /// configuration data during initialization.
+        /// </summary>
+        public string RootDirectory { get; set; }
+
+        /// <summary>
+        /// Initializes the provider during silo startup.
+        /// </summary>
+        /// <param name="name">The name of the provider</param>
+        /// <param name="storageProviderManager">A provider manager reference.</param>
+        /// <returns></returns>
+        public override Task Init(string name, IProviderRuntime providerRuntime, IProviderConfiguration config)
+        {
+            this.Name = name;
+            this.RootDirectory = config.Properties["RootDirectory"];
+            if (string.IsNullOrWhiteSpace(RootDirectory)) throw new ArgumentException("RootDirectory property not set");
+            DataManager = new GrainStateFileDataManager(RootDirectory);
+            return base.Init(name, providerRuntime, config);
+        }
+    }
+
+    /// <summary>
+    /// Interfaces with the file system.
+    /// </summary>
+    internal class GrainStateFileDataManager : IJSONStateDataManager 
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="storageDirectory">A path relative to the silo host process' working directory.</param>
+        public GrainStateFileDataManager(string storageDirectory)
+        {
+            directory = new DirectoryInfo(storageDirectory);
+            if (!directory.Exists)
+                directory.Create();
+        }
+
+        /// <summary>
+        /// Deletes a file representing a grain state object.
+        /// </summary>
+        /// <param name="typename">The type of the grain state object.</param>
+        /// <param name="fileName">The grain id string.</param>
+        public Task Delete(string collectionName, string key)
+        {
+            var fName = key + "." + collectionName;
+            var path = Path.Combine(directory.FullName, fName);
+
+            var fileInfo = new FileInfo(path);
+            if (fileInfo.Exists)
+                fileInfo.Delete();
+
+            return TaskDone.Done;
+        }
+
+        /// <summary>
+        /// Reads a file representing a grain state object.
+        /// </summary>
+        /// <param name="fileName">The grain id string.</param>
+        /// <param name="typename">The type of the grain state object.</param>
+        public async Task<string> Read(string collectionName, string key)
+        {
+            var fName = key + "." + collectionName;
+            var path = Path.Combine(directory.FullName, fName);
+
+            var fileInfo = new FileInfo(path);
+            if (!fileInfo.Exists)
+                return null;
+
+            using (var stream = fileInfo.OpenText())
+            {
+                return await stream.ReadToEndAsync();
+            }
+        }
+
+        /// <summary>
+        /// Writes a file representing a grain state object.
+        /// </summary>
+        /// <param name="fileName">The grain id string.</param>
+        /// <param name="typename">The type of the grain state object.</param>
+        public async Task Write(string collectionName, string key, string entityData)
+        {
+            string fileName = key + "." + collectionName;
+            var path = Path.Combine(directory.FullName, fileName);
+
+            var fileInfo = new FileInfo(path);
+
+            using (var stream = new StreamWriter(fileInfo.Open(FileMode.Create, FileAccess.Write)))
+            {
+                await stream.WriteAsync(entityData);
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+
+        private DirectoryInfo directory;
+    }
+}
