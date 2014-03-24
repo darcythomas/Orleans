419e3e64458f0f9f0dbdffcf926a12e9726eb857
Niklas Gustafsson <niklasg@microsoft.com>
2014-03-24 16:56:42 -0700

    Fixing CMD for Chirper demo

--- src/samples/Chirper/4.Demo Publisher.cmd
+++ src/samples/Chirper/4.Demo Publisher.cmd
@@ -5,7 +5,7 @@
 set CMDHOME=%~dp0.
 set USER=44444841
 
-if exist "%CMDHOME%\ChirperUI\bin\Debug\Chirper2012UI.exe" (
+if exist "%CMDHOME%\ChirperClient\bin\Debug\Chirper2012Client.exe" (
 "%CMDHOME%\ChirperClient\bin\Debug\Chirper2012Client.exe" /pub %USER%
 ) else (
 @echo Build Chirper2012.sln and then run the program
